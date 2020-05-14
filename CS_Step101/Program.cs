59e834a07195246a6bb655fae47a861282c637a9
Gavin Kimberlin <guviner@gmail.com>
2020-05-14 13:49:49 -0700

    Create a program that determines if someone is qualified for a position.

--- CS_Step101/Program.cs
+++ CS_Step101/Program.cs
@@ -10,6 +10,16 @@ namespace CS_Step101
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("What is your age?");
+            byte age = Convert.ToByte(Console.ReadLine());
+            Console.WriteLine("Have you ever had a DUI?");
+            bool dui = Convert.ToBoolean(Console.ReadLine());
+            Console.WriteLine("How many speeding tickets do you have?");
+            byte tickets = Convert.ToByte(Console.ReadLine());
+
+            bool qualified = age > 15 && dui == false && tickets <= 3;
+            Console.WriteLine("Qualified?\r\n" + qualified);
+            Console.ReadLine();
         }
     }
 }
